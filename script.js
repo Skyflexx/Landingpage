10d0b9e854924d354ba56205fbc901461882e4f7
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-04-05 16:47:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-let logoFb = document.getElementById("facebook")
-let logoInsta = document.getElementById("instagram")
-let logoTwit = document.getElementById("twitter")
+let logoFb = document.getElementById("frog")
 
 // Le but est de changer les logos noirs en logos blancs
 
 
 // Fonction pour changer le logo facebook
 
-function changeFb() {
-    logoFb.src = "https://img.icons8.com/ios-glyphs/30/fbd17e/facebook-new.png"
+function changeLogo() {
+    logoFb.src = "/img/frogorange.png"
 }
 
-function resetFb() {
-    logoFb.src = "https://img.icons8.com/ios-glyphs/30/FFFFFF/facebook-new.png"
-}
-
-// Fonction pour changer le logo instagram
-
-function changeInsta() {
-    logoInsta.src = "https://img.icons8.com/ios-glyphs/30/fbd17e/instagram-new.png"
-}
-
-function resetInsta() {
-    logoInsta.src = "https://img.icons8.com/ios-glyphs/30/FFFFFF/instagram-new.png"
+function resetLogo() {
+    logoFb.src = "/img/frognew.png"
 }
 
 
-// Fonction pour changer le logo twitter
-
-function changeTwit() {
-    logoTwit.src = "https://img.icons8.com/fluency-systems-regular/30/fbd17e/twitter.png"
-}
-
-function resetTwit() {
-    logoTwit.src = "https://img.icons8.com/fluency-systems-regular/30/FFFFFF/twitter.png"
-}
 
