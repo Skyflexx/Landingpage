4386f3b03e43ed327fce97ba1b37a2ced8956ac6
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-05-09 20:41:08 +0200

    add burger Btn

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ function resetLogo() {
     logoFb.src = "/img/frognew.png"
 }
 
+var sideNav = document.getElementById("sideNav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
 
+// récupération des éléments html via getElementById pour pouvoir les manipuler.
+
+// Appel des fct openNav et closeNav lorsqu'on appuie sur le menu ou sur la croix de fermeture
+
+openBtn.onclick = openNav; 
+closeBtn.onclick = closeNav;
+
+// openNav et closeNav ajoutent la classe "active" à la classe sideNav de base. Ce qui permet de modifier les propriétés de la navBar.
+
+function openNav(){
+    sideNav.classList.add("active"); // ajout de la partie "active" à la classe sideNav par concaténation.
+}
+
+function closeNav(){
+    sideNav.classList.remove("active"); // retrait de la partie "active" pour revenir à la classe normale.
+}
 
 
 
